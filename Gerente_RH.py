f02a20e395714ea485c4ddebe68707c78584db6e
Jayme-Vinicius <jayme.pedroza@hotmail.com>
2025-02-02 06:33:47 -0300

    Update Gerente_RH.py
    2.0

--- Gerente_RH.py
+++ Gerente_RH.py
@@ -1,26 +1,90 @@
-def build_profile(nome, idade, função, salario, horario, beneficios):
-    user_profile = {"nome":nome,'idade': idade, "função": função, 'salario': salario, 'horario': horario, 'beneficios': beneficios}
-    return user_profile    
+#import os.system("cls") from os (pra apagar os coisa deipóis kkkkk)
+#ver dicinarios e torcer para mudar tudo a tempo
 
-def profile_avaliation(nota, descrição, faltas):
-    user_avaliation = {"nota": nota, "descrição" : descrição, "faltas" : faltas}
-    return user_avaliation
+import Funções
 
-nome = input("Nome do novo funcionario: ").capitalize()
-idade = input("idade do novo funcionario: ")
-função = input("função do novo funcionario: ").capitalize()
-salario = input("salario do novo funcionario: ")
-horario = input("carga horaria do novo funcionario: ")
-beneficios = input("Beneficios do novo funcionario: ").capitalize()
-user_profile = build_profile(nome, idade, função, salario, horario, beneficios)
-print(user_profile)
-resposta = input("deseja avaliar o novo funcionario ? S ou N: ")
-if(resposta == 'S'):
-    nota = input("avalie o funcionario com uma nota: ")
-    descrição = input("descreva o funcionario: ").capitalize
-    faltas = input("quantas faltas o funcionario teve: ")
-    avaliação_do_funcionario = profile_avaliation(nota , descrição, faltas)
-    print(avaliação_do_funcionario)
-    print("funcionario registrado com sucesso ")
-else:
-    print("funcionario registrado com sucesso ")
+class Funcionários():
+    def __init__(self):
+        self.informações = Informação()
+        self.avaliação = Avaliação()
+        self.pagamento = Pagamento()
+        self.anterior_funcionário = None
+        self.proximo_funcionário = None
+
+class Informação():
+    def __init__(self):
+        self.nome = None
+        self.idade = None
+        self.cargo = None
+        self.salario_total = None
+        self.horarios = None
+        self.status = None
+
+class Avaliação():
+    def __init__(self):
+        self.nota = None
+        self.descrição = None
+        self.faltas = None
+
+class Pagamento():
+    def __init__(self):
+        self.salario_base = None
+        self.beneficios = None
+        self.descontos = None
+
+print("Olá seja bem vindo ao gerenciador de RH\n")
+lista_de_funcionários = None
+while (1):
+    choice_1 = (input("""
+Selecione a opção que desejar:
+Visualizar opções(0)\n\n"""))
+    match choice_1:
+        case '0':
+            print("""
+Visualizar sobre Funcionários(1)
+Visualizar Sobre Pagamentos, Beneficios e Descontos(2)
+Sair(-1)\n""")
+        case '1':
+            choice_2 = (input("""
+Visualizar Lista de Funcionários (1)
+Contratar Funcionários (2)
+Demitir Funcionários (3)
+Atualizar o Perfil dos Funcionários(4)
+Ver Avaliação Funcionários                              
+Avaliar os Funcionários(6)
+Voltar(-1)\n\n"""))
+            match choice_2:
+                case '1':
+                    lista_de_funcionários = Funções.Print_Informação_Funcionários(lista_de_funcionários)
+                case '2':
+                    lista_de_funcionários = Funções.Adicionar_funcionário(lista_de_funcionários)
+                case '3':
+                    lista_de_funcionários = Funções.Remover_Funcionário(lista_de_funcionários)
+                case '4':
+                     lista_de_funcionários = Funções.Atualizar_Informações(lista_de_funcionários)
+                case '5':
+                    lista_de_funcionários = Funções.Print_Avaliação_Funcionários(lista_de_funcionários)
+                case '6':
+                    lista_de_funcionários = Funções.Realizar_Avaliação_Funcionário(lista_de_funcionários)
+                case '-1':
+                    continue
+                case _:
+                    print("Opção invalida\n\n")
+        case '2':
+            choice_2 = (input("""
+Registrar Salario Total (1)
+Registrar Beneficios (2)
+Registrar Descontos (3)
+Voltar(-1)\n\n"""))
+            match choice_2:
+                case '1':
+                    lista_de_funcionários = Funções.Registrar_Salario_Total(lista_de_funcionários)
+                case '2':
+                    lista_de_funcionários = Funções.Registrar_Beneficios_Funcionario(lista_de_funcionários)
+                case '3':
+                    lista_de_funcionários = Funções.Registrar_Descontos_Funcionario(lista_de_funcionários)
+        case '-1':
+            print("Obrigado pela preferencia :)")
+            break
+        case '_':
+            print("Opção invalida")
